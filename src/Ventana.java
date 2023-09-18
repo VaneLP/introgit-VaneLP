7edb26c7eced43a4b5533cdb035f6c7804150cd1
Vane <123445952+VaneLP@users.noreply.github.com>
2023-09-18 11:45:13 +0200

    segundo commit

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,16 +1,37 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Ventana extends JFrame {
     private JPanel miPanel;
     private JButton button1;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
     private JButton button2;
 
     public Ventana(){
         setContentPane(miPanel);
         setTitle("Aplicacion inicial");
-        setSize(450,450);
+        //setSize(450,450);
+        pack();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"Boton pulsado. Gracias");
+            }
+        });
+        button2.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                super.mouseMoved(e);
+                JOptionPane.showMessageDialog(null,"Pasado por encima");
+            }
+        });
     }
 
     public static void main(String[] arg){
